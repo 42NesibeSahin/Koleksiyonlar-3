f2cd130a7c555f4673b99bf3da714ec0b8a306bd
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-15 16:09:32 +0300

    koleksiyonlar

---
+++ Koleksiyonlar_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] harfler = { 'a','e','ı','i','o','ö','u','ü'};
+            string metin;
+            Console.Write("Metin giriniz: ");
+            metin = Console.ReadLine();
+            ArrayList liste = new ArrayList();
+            for (int i = 0; i < harfler.Length; i++)
+            {
+                for (int j = 0; j < metin.Length; j++)
+                {
+                    if (metin[j] == harfler[i])
+                        liste.Add(harfler[i]);
+                }
+            }
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
